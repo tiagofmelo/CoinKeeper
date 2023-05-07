6adaa68cf12ea109d284bba9bb45dd67d0d43c52
Tiago Melo <Tiago.Melo@br.ey.com>
2023-05-07 17:26:26 -0300

    Add project files.

---
+++ PontoEletronico/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using PontoEletronico.Models;
+using PontoEletronico.Service;
+
+namespace PontoEletronico.Controllers
+{
+    public class HomeController : Controller
+    {
+        Pagination pagination;
+        List<TimesheetModel> timesheetModels = new List<TimesheetModel>();
+
+        public ActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = "Erro ao carregar a página \n" + erro.Message;
+                return RedirectToAction("Login", "Home");
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOut()
+        {
+            Response.Cookies.Remove("tokenModel");
+            Request.Cookies.Remove("tokenModel");
+            Response.Cookies.Remove("timesheetModel");
+            Request.Cookies.Remove("timesheetModel");
+
+            return RedirectToAction("Login", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Update()
+        {
+            try
+            {
+                if (Request.Form["evento"] != null && Request.Cookies["tokenModel"] != null)
+                {
+                    TokenModel tokenModel = PontoEletronico.Session.Cookie.RetornaToken(Request.Cookies["tokenModel"]);
+                    TimesheetModel timesheetModel = PontoEletronico.Session.Cookie.RetornaTimesheet(Request.Cookies["timesheetModel"]);
+
+                    switch (Request.Form["evento"])
+                    {
+                        case "1":
+                            TimesheetModel newTimesheet = new TimesheetModel
+                            {
+                                start = DateTime.Now,
+                            };
+
+                            var response = APIService.PostAPI(tokenModel, "Timesheet", newTimesheet);
+
+                            if (response.IsSuccessStatusCode)
+                                TempData["MensagemSucesso"] = "Registro efetuado com sucesso!";
+                            else
+                                TempData["MensagemErro"] = "Erro ao efetuar o registro!";
+
+                            break;
+                        case "2":
+                            timesheetModel.startLunch = DateTime.Now;
+
+                            response = APIService.PutAPI(tokenModel, "Timesheet", timesheetModel);
+
+                            if (response.IsSuccessStatusCode)
+                                TempData["MensagemSucesso"] = "Registro efetuado com sucesso!";
+                            else
+                                TempData["MensagemErro"] = "Erro ao efetuar o registro!";
+
+                            break;
+                        case "3":
+                            timesheetModel.endLunch = DateTime.Now;
+
+                            response = APIService.PutAPI(tokenModel, "Timesheet", timesheetModel);
+
+                            if (response.IsSuccessStatusCode)
+                                TempData["MensagemSucesso"] = "Registro efetuado com sucesso!";
+                            else
+                                TempData["MensagemErro"] = "Erro ao efetuar o registro!";
+
+                            break;
+                        case "4":
+                            timesheetModel.end = DateTime.Now;
+
+                            response = APIService.PutAPI(tokenModel, "Timesheet", timesheetModel);
+
+                            if (response.IsSuccessStatusCode)
+                                TempData["MensagemSucesso"] = "Registro efetuado com sucesso!";
+                            else
+                                TempData["MensagemErro"] = "Erro ao efetuar o registro!";
+
+                            break;
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+                return RedirectToAction("Login", "Home");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = "Erro ao registrar o ponto \n" + erro.Message;
+                return RedirectToAction("Login", "Home");
+            }
+        }
+    }
+}
---
+++ PontoEletronico/Models/TimesheetModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace PontoEletronico.Models
+{
+    public class TimesheetModel
+    {
+        public int id { get; set; }
+        public DateTime start { get; set; }
+        public DateTime startLunch { get; set; }
+        public DateTime endLunch { get; set; }
+        public DateTime end { get; set; }
+        public DateTime totalTime
+        {
+            get
+            {
+                try
+                {
+                    long totalTime;
+
+                    if (end.Ticks > 0)
+                    {
+                        totalTime = end.Subtract(start).Ticks;
+                    }
+                    else if (endLunch.Ticks > 0)
+                    {
+                        totalTime = endLunch.Subtract(start).Ticks;
+                    }
+                    else if (startLunch.Ticks > 0)
+                    {
+                        totalTime = startLunch.Subtract(start).Ticks;
+                    }
+                    else
+                    {
+                        totalTime = DateTime.MinValue.Ticks;
+                    }
+
+                    DateTime dateTime = new DateTime(totalTime);
+
+                    return dateTime;
+                }
+                catch 
+                {
+                    return DateTime.MinValue;
+                }
+            }
+        }
+    }
+
+    public class Pagination
+    {
+        public TimesheetModel[] items { get; set; }
+        public int count { get; set; }
+        public int currentPage { get; set; }
+        public int pageSize { get; set; }
+        public int totalPages { get; set; }
+        public string nextPage { get; set; }
+        public string previousPage { get; set; }
+    }
+}
